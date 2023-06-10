c2cdf88cd81606509d3fd021243c454dc8cd5abc
Syed Arsal Abbas <83390475+ArsalAbbas@users.noreply.github.com>
2023-06-10 10:02:31 +0530

    Create fourSum.cpp

---
+++ Day4/fourSum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+string fourSum(vector<int> arr, int target, int n) {
+    sort(arr.begin(),arr.end());
+  for (int i = 0; i < n; i++) {
+        if(i>0 && arr[i]==arr[i-1]) continue;
+     for (int j = i + 1; j < n; j++) {
+        if(j!=i+1 && arr[j]==arr[j-1]) continue;
+        int k=j+1;
+        int l=n-1;
+         while (k < l) {
+            long long sum=arr[i]+arr[j]+arr[k]+arr[l];
+            if(sum==target) return "Yes";
+            else if(sum>target) l--;
+            else k++;
+        }
+    }
+  }
+  return "No";
+}
